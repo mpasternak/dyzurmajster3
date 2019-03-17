5b04f4245a4c3ce8e8d344e1e2c9a0e7dc94b028
Michał Pasternak <michal.dtz@gmail.com>
2019-03-17 18:11:58 +0100

    Normalny port

--- src/dyzurmajster3/settings.py
+++ src/dyzurmajster3/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dyzurmajster3',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 
